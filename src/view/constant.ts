94754d9906b62f71b1b73d5b347396cbf7fef335
guchenfly <guchenfly@163.com>
2025-10-20 17:36:24 +0800

    修改播放文件地址

--- src/view/constant.ts
+++ src/view/constant.ts
@@ -22,21 +22,21 @@ export const course = [
         id:'103',
         description:true,
         // video: '1：23-0213正念睡眠班第3周课程跳出对失眠的担忧.mp4'
-        
+        video: 'https://nana-1318578439.cos.ap-beijing.myqcloud.com/%E6%AD%A3%E5%BF%B5%E7%9D%A1%E7%9C%A08%E5%91%A8%E8%AE%AD%E7%BB%83%E8%90%A5/1%EF%BC%9A23-0213%E6%AD%A3%E5%BF%B5%E7%9D%A1%E7%9C%A0%E7%8F%AD%E7%AC%AC3%E5%91%A8%E8%AF%BE%E7%A8%8B%E8%B7%B3%E5%87%BA%E5%AF%B9%E5%A4%B1%E7%9C%A0%E7%9A%84%E6%8B%85%E5%BF%A7.mp4'
       },
       {
         title:'【基础营】正念睡眠-宸宇老师（10月8日第4讲）',
         id:'104',
         description:true,
         // video: '1：16-0213正念睡眠班第4周课程睡不着怎么办.mp4'
-        video: 'https://nana-1318578439.cos.ap-beijing.myqcloud.com/%E6%AD%A3%E5%BF%B5%E7%9D%A1%E7%9C%A08%E5%91%A8%E8%AE%AD%E7%BB%83%E8%90%A5/1%EF%BC%9A16-0213%E6%AD%A3%E5%BF%B5%E7%9D%A1%E7%9C%A0%E7%8F%AD%E7%AC%AC%E5%9B%9B%E5%91%A8%E8%AF%BE%E7%A8%8B%E7%9D%A1%E4%B8%8D%E8%B5%B7%E6%80%8E%E4%B9%88%E5%8A%9E.mp4'
+        video: 'https://nana-1318578439.cos.ap-beijing.myqcloud.com/%E6%AD%A3%E5%BF%B5%E7%9D%A1%E7%9C%A08%E5%91%A8%E8%AE%AD%E7%BB%83%E8%90%A5/1%EF%BC%9A16-0213%E6%AD%A3%E5%BF%B5%E7%9D%A1%E7%9C%A0%E7%8F%AD%E7%AC%AC4%E5%91%A8%E8%AF%BE%E7%A8%8B%E7%9D%A1%E4%B8%8D%E7%9D%80%E6%80%8E%E4%B9%88%E5%8A%9E.mp4'
       },
       {
         title:'【基础营】正念睡眠-宸宇老师（10月8日第5讲）',
         id:'105',
         description:true,
         // video: '1：14-0213睡眠班第5周课程应对头脑层次的扰乱.mp4'
-        video: 'https://nana-1318578439.cos.ap-beijing.myqcloud.com/%E6%AD%A3%E5%BF%B5%E7%9D%A1%E7%9C%A08%E5%91%A8%E8%AE%AD%E7%BB%83%E8%90%A5/1%EF%BC%9A14-0213%E7%9D%A1%E7%9C%A0%E7%8F%AD%E7%AC%AC%E4%BA%94%E5%91%A8%E8%AF%BE%E7%A8%8B%E5%BA%94%E5%AF%B9%E5%A4%B4%E8%84%91%E5%B1%82%E6%AC%A1%E7%9A%84%E6%89%B0%E4%B9%B1.mp4'
+        video: 'https://nana-1318578439.cos.ap-beijing.myqcloud.com/%E6%AD%A3%E5%BF%B5%E7%9D%A1%E7%9C%A08%E5%91%A8%E8%AE%AD%E7%BB%83%E8%90%A5/1%EF%BC%9A14-0213%E7%9D%A1%E7%9C%A0%E7%8F%AD%E7%AC%AC5%E5%91%A8%E8%AF%BE%E7%A8%8B%E5%BA%94%E5%AF%B9%E5%A4%B4%E8%84%91%E5%B1%82%E6%AC%A1%E7%9A%84%E6%89%B0%E4%B9%B1.mp4'
       },
       {
         title:'【基础营】正念睡眠-宸宇老师（10月8日第6讲）',
@@ -47,17 +47,17 @@ export const course = [
       },
       {
         title:'【基础营】正念睡眠-宸宇老师（10月8日第7讲）',
-        id:'106',
+        id:'107',
         description:true,
         // video: '1：10-0213正念睡眠班第7周课程时时关照自己的身心.mp4'
-        video: 'https://nana-1318578439.cos.ap-beijing.myqcloud.com/%E6%AD%A3%E5%BF%B5%E7%9D%A1%E7%9C%A08%E5%91%A8%E8%AE%AD%E7%BB%83%E8%90%A5/1%EF%BC%9A10-0213%E6%AD%A3%E5%BF%B5%E7%9D%A1%E7%9C%A0%E7%8F%AD%E7%AC%AC%E4%B8%83%E5%91%A8%E8%AF%BE%E7%A8%8B%E6%97%B6%E6%97%B6%E5%85%B3%E7%85%A7%E8%87%AA%E5%B7%B1%E7%9A%84%E8%BA%AB%E5%BF%83.mp4'
+        video: 'https://nana-1318578439.cos.ap-beijing.myqcloud.com/%E6%AD%A3%E5%BF%B5%E7%9D%A1%E7%9C%A08%E5%91%A8%E8%AE%AD%E7%BB%83%E8%90%A5/1%EF%BC%9A10-0213%E6%AD%A3%E5%BF%B5%E7%9D%A1%E7%9C%A0%E7%8F%AD%E7%AC%AC7%E5%91%A8%E8%AF%BE%E7%A8%8B%E6%97%B6%E6%97%B6%E5%85%B3%E7%85%A7%E8%87%AA%E5%B7%B1%E7%9A%84%E8%BA%AB%E5%BF%83.mp4'
       },
       {
         title:'【基础营】正念睡眠-宸宇老师（10月8日第8讲）',
-        id:'106',
+        id:'108',
         description:true,
         // video: '1：7-0213正念睡眠班第8周课程回放养成正念睡眠的习惯.mp4'
-        video: 'https://nana-1318578439.cos.ap-beijing.myqcloud.com/%E6%AD%A3%E5%BF%B5%E7%9D%A1%E7%9C%A08%E5%91%A8%E8%AE%AD%E7%BB%83%E8%90%A5/1%EF%BC%9A7-0213%E6%AD%A3%E5%BF%B5%E7%9D%A1%E7%9C%A0%E7%8F%AD%E7%AC%AC%E5%85%AB%E5%91%A8%E8%AF%BE%E7%A8%8B%E5%9B%9E%E6%94%BE%E5%85%BB%E6%88%90%E6%AD%A3%E5%BF%B5%E7%9D%A1%E7%9C%A0%E7%9A%84%E4%B9%A0%E6%83%AF.mp4'
+        video: 'https://nana-1318578439.cos.ap-beijing.myqcloud.com/%E6%AD%A3%E5%BF%B5%E7%9D%A1%E7%9C%A08%E5%91%A8%E8%AE%AD%E7%BB%83%E8%90%A5/1%EF%BC%9A7-0213%E6%AD%A3%E5%BF%B5%E7%9D%A1%E7%9C%A0%E7%8F%AD%E7%AC%AC8%E5%91%A8%E8%AF%BE%E7%A8%8B%E5%9B%9E%E6%94%BE%E5%85%BB%E6%88%90%E6%AD%A3%E5%BF%B5%E7%9D%A1%E7%9C%A0%E7%9A%84%E4%B9%A0%E6%83%AF.mp4'
       }
     ]
   },
